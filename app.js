cd338a04860f02aad5d236e49cd99bbd404cbe6e
Debojyoti Ghosh <debojyotighosh@users.noreply.github.com>
2016-10-27 12:43:24 +0530

    Files added from local device

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$(function() {
+    $("#next").on("click", next)
+    $("#prev").on("click", prev)
+});
+
+function next() {
+    // Get current index
+    var index = $(".button-wrapper").attr("data-indexer");
+    index = parseInt(index);
+    console.log(index);
+
+    if(index >= 0) {
+        // Change data-indexer attribute
+        $(".button-wrapper").attr("data-indexer", index - 1);
+
+        // Assign class names to corresponding thumbnails
+        $("#thumb-" + index).attr("class", "thumbnail stack-out");
+        $("#thumb-" + (index - 1)).attr("class", "thumbnail stack-top");
+        $("#thumb-" + (index - 2)).attr("class", "thumbnail stack-intermediate-upper");
+        $("#thumb-" + (index - 3)).attr("class", "thumbnail stack-intermediate-lower");
+    }
+}
+
+function prev() {
+    // Get current index
+    var index = $(".button-wrapper").attr("data-indexer");
+    index = parseInt(index);
+    console.log(index);
+
+    if($('#thumb-' + (index + 1)).length > 0) {
+        // Change data-indexer attribute
+        $(".button-wrapper").attr("data-indexer", index + 1);
+
+        // Assign class names to corresponding thumbnails
+        $("#thumb-" + (index + 1)).attr("class", "thumbnail stack-top");
+        $("#thumb-" + index).attr("class", "thumbnail stack-intermediate-upper");
+        $("#thumb-" + (index - 1)).attr("class", "thumbnail stack-intermediate-lower");
+        $("#thumb-" + (index - 2)).attr("class", "thumbnail stack-bottom");
+    }
+}
